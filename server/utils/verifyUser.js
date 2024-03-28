bd7bf04cf232260ecba23c48f63a012829c0a164
Estra-Dev <dominikolyson@gmail.com>
2024-03-28 17:00:33 +0100

    change verifyUser

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -2,7 +2,8 @@ import jwt from "jsonwebtoken";
 import { errorHandler } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
+  // const token = req.cookies.access_token;
+  const token = req.cookies.cookies_accepted;
   console.log(token);
 
   if (!token) {
